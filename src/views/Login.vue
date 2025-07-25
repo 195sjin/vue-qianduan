82784c07b45d2eec77c25359c37fd0ff3683d165
xiaojin <zijin195@163.com>
2025-07-25 10:42:13 +0800

    主页面的搭建以及路由的使用

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -52,6 +52,9 @@ const register = async ()=>{
 //调用后台接口，完成登录。。。。绑定数据，复用注册表单的数据模型
 //表单数据校验
 //登录函数
+import { useRouter } from 'vue-router'
+const router = useRouter();
+
 const login=async()=>{
     let result=await userLoginService(registerData.value);
     /* if (result.code===0) {
@@ -61,6 +64,8 @@ const login=async()=>{
     } */
    //alert(result.msg ? result.msg : '登录成功')
    ElMessage.success(result.msg ? result.msg : '登录成功')
+   //登录成功，跳转页面，由路由完成
+   router.push('/')
 }
 
 //定义函数，清空数据模型的数据
